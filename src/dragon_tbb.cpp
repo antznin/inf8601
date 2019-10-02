d7b65f0cbe7581ee5c259f924712cc4c6a552487
Achleole <32515830+Achleole@users.noreply.github.com>
2019-10-02 11:04:34 -0400

    Implements dragon_tbb
    Mais bon c'est un peu trop du copier coller en l'etat.

--- src/dragon_tbb.cpp
+++ src/dragon_tbb.cpp
@@ -22,20 +22,127 @@ using namespace tbb;
 class DragonLimits {
 	public:
 	piece_t pieces[NB_TILES];
+    
+    void operator()(const blocked_range<size_t>& r ){
+	    int start = r.begin();
+	    int end = r.end();
+	    for (int i = 0; i < NB_TILES; i++) {
+		    piece_limit(start, end, &pieces[i]);
+	    }
+    }
+    
+    void join(const DragonLimits& other){
+	    for (int i = 0; i < NB_TILES; i++) {
+		    piece_merge(&pieces[i],other.pieces[i], tiles_orientation[i]);	
+	    }
+    }
+    
+    DragonLimits( DragonLimits& other, split){
+        for (int i = 0; i < NB_TILES; i++) {
+		    piece_init(& pieces[i]);
+		    pieces[i].orientation = tiles_orientation[i];   
+        }     
+    }
+    DragonLimits(){
+        for (int i = 0; i < NB_TILES; i++) {
+		    piece_init(& pieces[i]);
+		    pieces[i].orientation = tiles_orientation[i];   
+        }     
+    }
+
 };
 
-class DragonDraw {
+
+class DragonClear
+{
+  public:
+	DragonClear(const DragonClear &d) { this->_draw_data = d._draw_data; }
+	DragonClear(draw_data *draw_data) { this->_draw_data = draw_data; }
+	void operator()(const blocked_range<uint64_t> &r) const
+	{
+		init_canvas(r.begin(), r.end(), this->_draw_data->dragon, -1);
+	}
+  private:
+	draw_data *_draw_data;
 };
 
-class DragonRender {
+class DragonDraw
+{
+  public:
+	DragonDraw(const DragonDraw &d, TidMap *tidMap)
+	{
+		this->_draw_data = d._draw_data;
+		this->_tidMap = tidMap;
+	}
+	DragonDraw(draw_data *draw_data, TidMap *tidMap)
+	{
+		this->_draw_data = draw_data;
+		this->_tidMap = tidMap;
+	}
+
+	void operator()(const blocked_range<uint64_t> &range) const
+	{
+		xy_t position;
+		xy_t orientation;
+		uint64_t n;
+		for (size_t k = 0; k < NB_TILES; k++)
+		{
+			//dragon_draw_raw(k, range.begin(), range.end(), this->_draw_data->dragon, this->_draw_data->dragon_width, this->_draw_data->dragon_height, this->_draw_data->limits, _tidMap->getIdFromTid(gettid()), 1);
+			
+            position = compute_position(k, range.begin(), 1);
+			orientation = compute_orientation(k, range.begin(), 1);
+			position.x -= this->_draw_data->limits.minimums.x;
+			position.y -= this->_draw_data->limits.minimums.y;
+
+			for (n = range.begin() + 1; n <= range.end(); n++)
+			{
+				int j = (position.x + (position.x + orientation.x)) >> 1;
+				int i = (position.y + (position.y + orientation.y)) >> 1;
+				int index = i * this->_draw_data->dragon_width + j;
+
+				this->_draw_data->dragon[index] = n * this->_draw_data->nb_thread / this->_draw_data->size;
+
+				position.x += orientation.x;
+				position.y += orientation.y;
+				if (((n & -n) << 1) & n)
+					rotate_left(&orientation);
+				else
+					rotate_right(&orientation);
+			}//*/
+		}
+	}
+
+  private:
+	TidMap *_tidMap;
+	draw_data *_draw_data;
 };
 
-class DragonClear {
+class DragonRender
+{
+  public:
+	DragonRender(const DragonRender &d) { this->_draw_data = d._draw_data; }
+	DragonRender(draw_data *draw_data) { this->_draw_data = draw_data; }
+
+	void operator()(const blocked_range<uint64_t> &range) const
+	{
+		scale_dragon(range.begin(),
+					 range.end(),
+					 this->_draw_data->image,
+					 this->_draw_data->image_width,
+					 this->_draw_data->image_height,
+					 this->_draw_data->dragon,
+					 this->_draw_data->dragon_width,
+					 this->_draw_data->dragon_height,
+					 this->_draw_data->palette);
+	}
+
+  private:
+	draw_data *_draw_data;
 };
+
 
 int dragon_draw_tbb(char **canvas, struct rgb *image, int width, int height, uint64_t size, int nb_thread)
 {
-	TODO("dragon_draw_tbb");
 	struct draw_data data;
 	limits_t limits;
 	char *dragon = NULL;
@@ -53,9 +160,8 @@ int dragon_draw_tbb(char **canvas, struct rgb *image, int width, int height, uin
 
 	/* 1. Calculer les limites du dragon */
 	dragon_limits_tbb(&limits, size, nb_thread);
-
 	task_scheduler_init init(nb_thread);
-	
+
 	dragon_width = limits.maximums.x - limits.minimums.x;
 	dragon_height = limits.maximums.y - limits.minimums.y;
 	dragon_surface = dragon_width * dragon_height;
@@ -65,8 +171,9 @@ int dragon_draw_tbb(char **canvas, struct rgb *image, int width, int height, uin
 	deltaJ = (scale * width - dragon_width) / 2;
 	deltaI = (scale * height - dragon_height) / 2;
 
-	dragon = (char *) malloc(dragon_surface);
-	if (dragon == NULL) {
+	dragon = (char *)malloc(dragon_surface);
+	if (dragon == NULL)
+	{
 		free_palette(palette);
 		return -1;
 	}
@@ -84,18 +191,34 @@ int dragon_draw_tbb(char **canvas, struct rgb *image, int width, int height, uin
 	data.deltaI = deltaI;
 	data.deltaJ = deltaJ;
 	data.palette = palette;
-	data.tid = (int *) calloc(nb_thread, sizeof(int));
+	data.tid = (int *)calloc(nb_thread, sizeof(int));
 
 	/* 2. Initialiser la surface : DragonClear */
+	DragonClear dragon_clear(&data);
+	parallel_for(blocked_range<uint64_t>(0, dragon_surface), dragon_clear);
 
 	/* 3. Dessiner le dragon : DragonDraw */
 
+	TidMap *tidMap = new TidMap(nb_thread);
+
+	DragonDraw dragon_draw(&data, tidMap);
+	parallel_for(blocked_range<uint64_t>(0, data.size), dragon_draw);
+
 	/* 4. Effectuer le rendu final */
+	DragonRender dragon_render(&data);
+	parallel_for(blocked_range<uint64_t>(0, data.image_height), dragon_render);
+
+	//Décommenter pour la partie 3
+	//cout << "Total intervals:\t" << counter << endl;
 
 	free_palette(palette);
 	FREE(data.tid);
-	//*canvas = dragon;
-	*canvas = NULL; // TODO: Retourner le dragon calculé
+	
+	//Décommenter pour la partie 3
+	//tidMap->dump();
+	
+	FREE(tidMap);
+	*canvas = dragon;
 	return 0;
 }
 
@@ -105,11 +228,13 @@ int dragon_draw_tbb(char **canvas, struct rgb *image, int width, int height, uin
  */
 int dragon_limits_tbb(limits_t *limits, uint64_t size, int nb_thread)
 {
-	TODO("dragon_limits_tbb");
 	DragonLimits lim;
 
-	/* 1. Calculer les limites */
+    
+    tbb::task_scheduler_init init(nb_thread);
 
+	/* 1. Calculer les limites */
+    parallel_reduce(blocked_range<size_t>(0,size), lim);
 	/* La limite globale est calculée à partir des limites
 	 * de chaque dragon.
 	 */
